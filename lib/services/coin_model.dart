e7c1cb03d6549f8730b9526379b01d56ff105208
Iury Vasconcelos <iury0393@gmail.com>
2020-05-05 18:18:07 -0300

    Criando o mapa para retornar os valores de outras cryptomoedas

--- lib/services/coin_model.dart
+++ lib/services/coin_model.dart
@@ -1,17 +1,21 @@
+import 'package:bitcoin_ticker/utilities/constants.dart';
 import 'networking.dart';
 
-const apiKey = '6E2A00B1-C020-485F-A4EB-576BE1454141';
+const apiKey = 'YOUR-API-KEY-HERE';
 const coinAPIUrl = 'https://rest.coinapi.io/v1/exchangerate';
 
 class CoinModel {
   Future<dynamic> getExchange(String currency) async {
-    //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
-    //TODO 5: Return a Map of the results instead of a single value.
-    NetworkHelper networkHelper =
-        NetworkHelper('$coinAPIUrl/BTC/$currency?apikey=$apiKey');
+    Map<String, String> cryptoPrices;
 
-    var coinData = await networkHelper.getData();
-    var lastPrice = coinData['rate'];
-    return lastPrice;
+    for (String crypto in kCryptoList) {
+      NetworkHelper networkHelper =
+          NetworkHelper('$coinAPIUrl/$crypto/$currency?apikey=$apiKey');
+      var coinData = await networkHelper.getData();
+      var lastPrice = coinData['rate'];
+      cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+    }
+
+    return cryptoPrices;
   }
 }
